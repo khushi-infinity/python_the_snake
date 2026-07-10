5a843f69bb8c984dda64eb63e76ea6e9a2a4d2c8
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-10 12:27:32 +0530

    datatypes

--- thedemo/script.py
+++ thedemo/script.py
@@ -7,5 +7,20 @@ print("Hello" + " is it space")
 # Enter your name
 firstname = input("What is your name? \n")
 print(len(firstname))
+# print(len(input("your name?")))
+
 print("Hello "+ firstname)
 print("Hello "+input("What is your full name? \n") + "!")
+
+#  what's your type?
+print(type("Hello"))
+print(type(1234))
+print(type(12.34))
+
+# Type casting
+print(type(int("342")))
+# int(), float(), str(), bool()
+
+# float and int
+type(6/3) # float
+type(6//3) # int
