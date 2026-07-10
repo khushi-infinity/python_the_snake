d3b55952907f2379b3d0aa5eb8a6e920f3c446c0
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-10 10:31:30 +0530

    the beginning of the python

---
+++ script.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("This will go\nnew line yo!")
+
+# string concatenation
+print("Hello" + " is it space")
+
+# Enter your name
+firstname = input("What is your name? \n")
+print(len(firstname))
+print("Hello "+ firstname)
+print("Hello "+input("What is your full name? \n") + "!")
