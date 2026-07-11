c408ba06dc3a46cbc1595169333df8567d526405
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-11 13:41:34 +0530

    loops

---
+++ thedemo/merry_go_round.py
@@ -0,0 +1,11 @@
+print('\n--------------------list of fruits in my basket!-------------------------')
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie")
+
+print(fruits)
+
+for num in range(1,11,3):
+    print(num)
