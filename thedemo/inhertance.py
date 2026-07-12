9a131a10f516f653a81aa96af258f85a0528f874
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-12 20:26:46 +0530

    Proper inheritance

--- thedemo/inhertance.py
+++ thedemo/inhertance.py
@@ -1,14 +1,26 @@
 #Animal class or Parent class
 class Animal:
     def __init__(self):
-        print("Animal")
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale!")
 
 
 # Child class inherits from the Animal class
 class Fish(Animal):
     def __init__(self):
         super().__init__()
-        print("Fish")
+
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater")
+
+    def swim(self):
+        print("moving in water")
 
 # Creating the fish object
 golden_fish = Fish()
+golden_fish.swim()
+golden_fish.breathe()
+print(golden_fish.num_eyes)
