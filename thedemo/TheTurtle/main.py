a841324bb5afd5bd9d62ed27ce27a1cddc945aed
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-12 17:38:36 +0530

    turtle makes a square

--- thedemo/TheTurtle/main.py
+++ thedemo/TheTurtle/main.py
@@ -2,7 +2,18 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("red")
+timmy.color("green")
+
+def forward_turn_left():
+    timmy.forward(100)
+    timmy.left(90)
+
+def make_a_square():
+    for i in range(4):
+        forward_turn_left()
+
+
+make_a_square()
 
 screen = Screen()
 screen.exitonclick()
