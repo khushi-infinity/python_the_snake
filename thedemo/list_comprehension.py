c30f9daec954b6412a17467aa5ef65dbc10fa140
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-13 10:00:47 +0530

    Dictionary comprehension

--- thedemo/list_comprehension.py
+++ thedemo/list_comprehension.py
@@ -23,3 +23,14 @@ print(short_names)
 
 upper_names = [name.upper() for name in names if len(name)>5]
 print(upper_names)
+
+
+# Dictionary Comprehension
+# new_dict = {new_key:new_value for item in list}
+# new_dict = {new_value:new_value for (key,value) in dict.items()}
+
+import random
+student_scores = {student:random.randint(1,100) for student in names}
+print(student_scores)
+passed_students = {student:score for (student, score) in student_scores.items() if score>=50}
+print(passed_students)
