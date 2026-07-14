cb782d68e53942324025af1994f68bf52ffaf675
Khushi Sarawagi <khushikhush006@gmail.com>
2026-07-14 13:49:27 +0530

    routing in flask

--- thedemo/HelloFlask/main.py
+++ thedemo/HelloFlask/main.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return ("<h1 style='Bold'>"
-            "<center>Hello, World!</center>"
-            "</h1>")
+    return render_template("index.html")
 
-@app.route("/username/<name>")
-def username(name):
-    return f"Hello {name}!"
+
+# @app.route("/bye")
+# def bye():
+#     return "Bye!"
+
+
+
+# @app.route("/username/<name>")
+# def username(name):
+#     return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
